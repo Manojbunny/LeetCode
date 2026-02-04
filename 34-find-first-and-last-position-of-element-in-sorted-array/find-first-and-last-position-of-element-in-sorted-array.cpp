2de623209fa0bbf647069a78621f5d0ec8eda6f2
Vema Manoj Kumar <125050677+Manojbunny@users.noreply.github.com>
2026-02-04 19:45:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (87.00%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -31,21 +31,27 @@ private:
         return first;
     }
     int findLast(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
+        int low = 0;
+        int high = nums.size() - 1;
         int last = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+        while(low <= high)
+        {
+            int mid = low + (high - low)/ 2;
+            if(nums[mid] == target)
+            {      
                 last = mid;
-                left = mid + 1;
+                low = mid + 1;
             }
-            else if (nums[mid] < target) {
-                left = mid + 1;
+            else if(nums[mid] < target)
+            {
+                low = mid + 1;
             }
-            else {
-                right = mid - 1;
+            else
+            {
+                high = mid - 1;
             }
         }
+        
         return last;
     }
 };
